9e3878460b62eb5f9b97c5d1d88155bad29b3c9f
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-07-23 16:42:44 -0400

    reformat to both sample sites.

--- sample/src/App.js
+++ sample/src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="blog/"
           target="_blank"
           rel="noopener noreferrer"
         >
